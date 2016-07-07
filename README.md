df3e05ed1c7b5ced0266c4cb2d68cb1de78746ec
Timi Wahalahti <timi.wahalahti@gmail.com>
2016-07-08 00:08:26 +0300

    Release version 0.1.0

---
+++ README.md
@@ -0,0 +1,99 @@
+# Dude Tweets feed
+WordPress plugin to get latest tweets for user and/or hashtag.
+
+Basically this plugin fetches tweets from Twitter, saves those to transient and next requests will be served from there. After transient has expired and deleted, new fetch from Twitter will be made and saved to transient. This implements very simple cache.
+
+Handcrafted with love at [Digitoimisto Dude Oy](http://dude.fi), a Finnish boutique digital agency in the center of Jyväskylä.
+
+Plugin uses [twitteroauth library](https://github.com/abraham/twitteroauth) made by [Abraham Williams](https://abrah.am/).
+
+## Table of contents
+1. [Please note before using](#please-note-before-using)
+2. [License](#license)
+3. [Usage](#usage)
+  1. [Hooks](#hooks)
+4. [Composer](#composer)
+5. [Contributing](#contributing)
+
+### Please note before using
+This plugin is not meant to be "plugin for everyone", it needs at least some basic knowledge about php and css to add it to your site and making it look beautiful.
+
+This is a plugin in development for now, so it may update very often.
+
+### License
+Dude Tweets feed is released under the GNU GPL 2 or later.
+
+### Usage
+This plugin does not have settings page or provide anything visible on front-end. So it's basically dumb plugin if you don't use any filters listed below.
+
+Use of [authentication](#authentication) filters is mandatory.
+
+Get user tweets by calling function `dude_twitter_feed()->get_user_tweets()`, pass username as a only argument.
+Get hashtag tweets by calling function `dude_twitter_feed()->get_hashtag_tweets()`, pass hashtag as a only argument.
+
+#### Hooks
+All the settings are set with filters, and there is also few filters to change basic functionality and manipulate data before caching.
+
+##### Authentication
+Authentication is made with four different strings, you can obtain all of those from Twitter [application manager](https://apps.twitter.com/).
+
+Filters are `dude-twitter-feed/oauth_consumer_key`, `dude-twitter-feed/oauth_consumer_secret`, `dude-twitter-feed/oauth_access_token` and `dude-twitter-feed/oauth_access_token_secret`.
+
+Defaults to empty string.
+
+##### `dude-twitter-feed/user_tweets_transient`
+Change name of the transient for user tweets, must be unique for every user. Passed arguments are default name and username.
+
+Defaults to `dude-twitter-user-$username`.
+
+##### `dude-twitter-feed/user_tweets_endpoint`
+Change endpoint for user tweets fetch. Only passed argument is default endpoint.
+
+Defaults to `statuses/user_timeline`.
+
+##### `dude-twitter-feed/user_tweets_parameters`
+Modify api call parameters, example count of results. Only passed argument is array of default parameters.
+
+Defaults to result count of five, retweet include and user information trimming.
+
+Possible parameters are listed in Twitter [documentation](https://dev.twitter.com/rest/reference/get/statuses/user_timeline).
+
+##### `dude-twitter-feed/user_tweets`
+Manipulate or use data before it's cached to transient. Only passed argument is array of tweets.
+
+##### `dude-twitter-feed/user_tweets_lifetime`
+Change activity cache lifetime. Only passed argument is default lifetime in seconds.
+
+Defaults to 600 (= ten minutes).
+
+##### `dude-twitter-feed/hashtag_tweets_transient`
+Change name of the transient for hashtag tweets, must be unique for every hashtag. Passed arguments are default name and hashtag.
+
+Defaults to `dude-twitter-hashtag-$hashtag`.
+
+##### `dude-twitter-feed/user_tweets_endpoint`
+Change endpoint for hashtag tweets fetch. Only passed argument is default endpoint.
+
+Defaults to `search/tweets`.
+
+##### `dude-twitter-feed/hashtag_tweets_parameters`
+Modify api call parameters, example count of results. Only passed argument is array of default parameters.
+
+Defaults to result count of five.
+
+Possible parameters are listed in Twitter [documentation](https://dev.twitter.com/rest/reference/get/search/tweets).
+
+##### `dude-twitter-feed/hashtag_tweets`
+Manipulate or use data before it's cached to transient. Only passed argument is array of tweets.
+
+##### `dude-twitter-feed/hashtag_tweets_lifetime`
+Change activity cache lifetime. Only passed argument is default lifetime in seconds.
+
+Defaults to 600 (= ten minutes).
+
+### Composer
+
+To use with composer, run `composer require digitoimistodude/dude-twitter-feed dev-master` in your project directory or add `"digitoimistodude/dude-twitter-feed":"dev-master"` to your composer.json require.
+
+### Contributing
+If you have ideas about the theme or spot an issue, please let us know. Before contributing ideas or reporting an issue about "missing" features or things regarding to the nature of that matter, please read [Please note section](#please-note-before-using). Thank you very much.
---
+++ dude-twitter-feed.php
@@ -0,0 +1,168 @@
+<?php
+/**
+ * Plugin Name: Dude Tweets feed
+ * Plugin URL: https://www.dude.fi
+ * Description: Fetches the latest tweets for spesified users and hashtgs
+ * Version: 0.1.0
+ * Author: Timi Wahalahti / DUDE
+ * Author URL: http://dude.fi
+ * Requires at least: 4.4.2
+ * Tested up to: 4.4.2
+ *
+ * Text Domain: dude-twitter-feed
+ * Domain Path: /languages
+ */
+
+if( !defined( 'ABSPATH' )  )
+	exit();
+
+require 'twitteroauth/autoload.php';
+use Abraham\TwitterOAuth\TwitterOAuth;
+
+Class Dude_Twitter_Feed {
+  private static $_instance = null;
+
+  /**
+   * Construct everything and begin the magic!
+   *
+   * @since   0.1.0
+   * @version 0.1.0
+   */
+  public function __construct() {
+    // Add actions to make magic happen
+    add_action( 'init', array( $this, 'load_plugin_textdomain' ) );
+  } // end function __construct
+
+  /**
+   *  Prevent cloning
+   *
+   *  @since   0.1.0
+   *  @version 0.1.0
+   */
+  public function __clone () {
+		_doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?', 'dude-twitter-feed' ) );
+	} // end function __clone
+
+  /**
+   *  Prevent unserializing instances of this class
+   *
+   *  @since   0.1.0
+   *  @version 0.1.0
+   */
+  public function __wakeup() {
+    _doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?', 'dude-twitter-feed' ) );
+  } // end function __wakeup
+
+  /**
+   *  Ensure that only one instance of this class is loaded and can be loaded
+   *
+   *  @since   0.1.0
+   *  @version 0.1.0
+	 *  @return  Main instance
+   */
+  public static function instance() {
+    if( is_null( self::$_instance ) ) {
+      self::$_instance = new self();
+    }
+
+    return self::$_instance;
+  } // end function instance
+
+  /**
+   *  Load plugin localisation
+   *
+   *  @since   0.1.0
+   *  @version 0.1.0
+   */
+  public function load_plugin_textdomain() {
+    load_plugin_textdomain( 'dude-twitter-feed', false, dirname( plugin_basename( __FILE__ ) ).'/languages/' );
+  } // end function load_plugin_textdomain
+
+	public function get_user_tweets( $twitter_handle = '' ) {
+		if( empty( $twitter_handle ) )
+			return;
+
+		$transient_name = apply_filters( 'dude-twitter-feed/user_tweets_transient', 'dude-twitter-user-'.$twitter_handle, $twitter_handle );
+		$tweets = get_transient( $transient_name );
+	  if( !empty( $tweets ) || false != $tweets )
+	    return $tweets;
+
+		$endpoint = apply_filters( 'dude-twitter-feed/user_tweets_endpoint', 'statuses/user_timeline' );
+		$parameters = array(
+			'screen_name'	=> $twitter_handle,
+			'count'				=> '5',
+			'include_rts'	=> 'true',
+			'trim_user'		=> true,
+		);
+
+		$response = self::_call_api( $endpoint, apply_filters( 'dude-twitter-feed/user_tweets_parameters', $parameters ) );
+		if( $response === FALSE )
+			return;
+
+		$response = apply_filters( 'dude-twitter-feed/user_tweets', $response );
+		set_transient( $transient_name, $response, apply_filters( 'dude-twitter-feed/user_tweets_lifetime', '600' ) );
+
+		return $response;
+	} // end function get_users_tweets
+
+	public function get_hashtag_tweets( $hashtag = '' ) {
+		if( empty( $hashtag ) )
+			return;
+
+		$transient_name = apply_filters( 'dude-twitter-feed/hashtag_tweets_transient', 'dude-twitter-hashtag-'.$hashtag, $hashtag );
+		$tweets = get_transient( $transient_name );
+	  if( !empty( $tweets ) || false != $tweets )
+	    return $tweets;
+
+		$endpoint = apply_filters( 'dude-twitter-feed/hashtag_tweets_endpoint', 'search/tweets' );
+		$parameters = array(
+			'q'			=> $hashtag,
+			'count'	=> '5',
+		);
+
+		$response = self::_call_api( $endpoint, apply_filters( 'dude-twitter-feed/hashtag_tweets_parameters', $parameters ) );
+		if( $response === FALSE )
+			return;
+
+		$response = apply_filters( 'dude-twitter-feed/hashtag_tweets', $response );
+		set_transient( $transient_name, $response, apply_filters( 'dude-twitter-feed/hashtag_tweets_lifetime', '600' ) );
+
+		return $response;
+	} // end function get_hashtag_tweets
+
+	private function _call_api( $endpoint = '', $parameters = array() ) {
+		if( empty( $endpoint ) || empty( $parameters ) )
+			return false;
+
+		$connection = new TwitterOAuth(
+			apply_filters( 'dude-twitter-feed/oauth_consumer_key', '' ),
+			apply_filters( 'dude-twitter-feed/oauth_consumer_secret', '' ),
+			apply_filters( 'dude-twitter-feed/oauth_access_token', '' ),
+			apply_filters( 'dude-twitter-feed/oauth_access_token_secret', '' )
+		);
+
+		$content = $connection->get("account/verify_credentials");
+		$response = $connection->get( $endpoint, $parameters );
+
+		if( $connection->getLastHttpCode() !== 200 ) {
+			self::_write_log( 'response status code not 200 OK, function: '.$endpoint );
+			return false;
+		}
+
+		return $response;
+	} // end function _call_api
+
+	private function _write_log ( $log )  {
+    if( true === WP_DEBUG ) {
+      if( is_array( $log ) || is_object( $log ) ) {
+        error_log( print_r( $log, true ) );
+      } else {
+        error_log( $log );
+      }
+    }
+  } // end _write_log
+} // end class Dude_Twitter_Feed
+
+function dude_twitter_feed() {
+  return new Dude_Twitter_Feed();
+} // end function dude_twitter_feed
